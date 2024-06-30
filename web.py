d47c231b2d69277c2696c4c09e826de5f2a9571d
David Bures <dabures@microsoft.com>
2024-06-29 21:26:15 -0700

    adding web server to display stored messages

---
+++ web.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template_string
+from db_operations import get_messages  # Assuming this function is in db_operations.py
+
+app = Flask(__name__)
+
+HTML_TEMPLATE = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Messages</title>
+</head>
+<body>
+    <h1>Messages</h1>
+    <ul>
+    {% for message in messages %}
+        <li>
+            <strong>[{{ message[3] }}]{{ message[1] }} ({{ message[2] }})</strong>: {{ message[5] }}
+        </li>
+    {% endfor %}
+    </ul>
+</body>
+</html>
+"""
+
+@app.route('/')
+def show_messages():
+    messages = get_messages(100)
+    return render_template_string(HTML_TEMPLATE, messages=messages)
+
+def create_app():
+   return app
+
+if __name__ == '__main__':
+    #app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+
+# run waitress-serve --port=8080 --call web:create_app to start the web server
